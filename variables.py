591d63f1711c999c7ecab415d0210b53c4c257fc
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-05-17 12:09:58 +0545

    First commit

---
+++ variables.py
@@ -0,0 +1,10 @@
+# 1 Variable is a name that is used to store data in a program.It acts like a container which hold values
+# 2 Variables can contain alphabest, digit,underscore
+# 3.Varibales cant start with digit
+# 4.varibales name cant contain spaces.
+# in python, it doesnt used {} braces, it used indentation(Which means spaces or tab at the beginning of a line in python)
+name = "Samiksha"
+age = 20 
+marks = 90
+print(name)
+print(age)
