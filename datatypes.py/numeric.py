59d3ffcae8055f3efed5b4723f77f4a3664f37b5
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-05-17 12:52:56 +0545

    First commit of datatypes

---
+++ datatypes.py/numeric.py
@@ -0,0 +1,19 @@
+# Datatypes  it tells which types of data should be store like integer, float, string(letter,word,semtence), Boolean , list, tuple, dictionary (dict).
+# A data type used to store whole numbers (without decimals) = integer
+# A data type used to store decimal numbers. = float
+# A data type used to store text (letters, words, sentences). = string
+# A data type that stores only True or False values.= Boolean
+# A data type used to store multiple items in a single variable (can be changed). = list
+# A data type used to store multiple items but cannot be changed. = tuple
+# A data type used to store data in key-value pairs. = dictionary(dict)
+
+
+# numeric types is used to find the types of numbers 
+
+age = 20
+t = type(age)
+print(t)
+
+price = 99.9
+print(type(price))
+
