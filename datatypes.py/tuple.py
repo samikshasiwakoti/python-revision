929321de37240c3d2a5c0dd3c7ee014cedf8999b
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-05-17 20:43:55 +0545

    This is about tuple datatype

---
+++ datatypes.py/tuple.py
@@ -0,0 +1,17 @@
+# tuple is a collection of multiple item s in single varibale like list but it is imuutable, and it uses parenthess
+
+num = (1,2,2,2,2,3,4,5)
+print(num.count(2))
+
+print(num.index(4))
+
+# converting tuple to list
+num1 = (2,3,4,5,6)
+new_list =list(num1)
+print(new_list)
+
+
+num2 = [1,2,3,4,5,6,]
+new_tuple = tuple(num2)
+print(new_tuple)
+
