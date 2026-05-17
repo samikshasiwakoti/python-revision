0c968751a9610b5cc7ab2f004d9fef30b99c4da7
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-05-17 13:19:44 +0545

    Second commit of datatypes

---
+++ datatypes.py/string.py
@@ -0,0 +1,18 @@
+# # String is a datatype in python. It is a sequence of characters enclosed in quotes.
+# # String is immutable, we cant change existing string
+# #  String Slicing in python can be sliced for getting a part of the strings.
+# Name =”Harry” we can do indexing , indexing always start with 0 , 1,2,3,4,5,
+# And we can do REVERSE Indexing from backward also and it start with (-1,-2,-3,-4,-5).
+# S1 = name[intd-satrt:ind_end]
+
+# doing slicing
+name = "samiksha"
+print(name[0:5])
+print(name[0])
+print(name[:3])
+print(name[3:])
+print(name[-2:-4])
+print(name[::-1])
+
+
+
