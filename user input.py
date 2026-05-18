9f02e7d8ed48aa5abedc9b3cec554e9a34f404af
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-05-18 17:35:32 +0545

    This is simple demo of how we take input form user

---
+++ user input.py
@@ -0,0 +1,9 @@
+#to give input by user we used input function
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+faculty = input("Enter faculty:")
+
+print(name)
+print(age)
+print(faculty)
+print(f"Welcome {name} in faculty {faculty}")
