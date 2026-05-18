efdfcacc27df364cd9525cb082f9c73370426bc7
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-05-18 18:28:37 +0545

    This is commit of conditionals statement

---
+++ conditionals/if.py
@@ -0,0 +1,28 @@
+#Conditionals are used to make decisions in programs
+# we have if,elif,else
+
+age = 15
+
+if age > 14:
+    print("It is valid age")
+
+else :
+    print("It is not valid")
+
+
+marks = int(input("Enter your marks"))    
+
+if marks >= 90:
+    print(" Grade A+")
+
+elif marks >=80 :
+    print(" Grade A")    
+
+elif marks >=70 :
+    print(" Grade B+") 
+
+elif marks >=60 :
+    print("Grade B")
+
+else :
+    print("u are fail")    
