870c920c94a9ce0d3896ebd74032a4b17b7796f6
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-05-18 17:54:59 +0545

    This is commit of type conversion

---
+++ Typeconversion.py
@@ -0,0 +1,16 @@
+age = " 20"
+print(str(age))
+
+number = 10
+print(int(number))
+
+number1 = 71.0
+converted = int(number1)
+print(converted)
+print(type(converted))
+
+
+num = 212
+converted1 = float(num)
+print(converted1)
+print(type(converted))
